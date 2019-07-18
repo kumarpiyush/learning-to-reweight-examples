5d85d22982b2cf3c41bff5e3dff1660450b60b84
Piyush <piyushkumar1523@gmail.com>
2019-07-18 11:58:54 -0700

    LeNet works

--- mnist_pt/model.py
+++ mnist_pt/model.py
@@ -9,6 +9,9 @@ class LeNet(nn.Module) :
         self.conv2 = nn.Conv2d(16, 32, 5, padding=2)
         self.conv3 = nn.Conv2d(32, 64, 5, padding=2)
 
+        self.fc4 = nn.Linear(1024, 100)
+        self.fc5 = nn.Linear(100, 10)
+
     def forward(self, x) :
         tval = self.conv1(x)
         tval = F.max_pool2d(tval, kernel_size=3, stride=2, padding=1)
@@ -21,3 +24,10 @@ class LeNet(nn.Module) :
         tval = self.conv3(tval)
         tval = F.max_pool2d(tval, kernel_size=3, stride=2, padding=1)
         tval = F.relu(tval)
+
+        tval = tval.view(-1, 1024)
+
+        tval = F.relu(self.fc4(tval))
+        logits = F.relu(self.fc5(tval))
+
+        return logits
